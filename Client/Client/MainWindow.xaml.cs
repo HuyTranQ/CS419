e7cefa9f0c90e5412b2b91c23f2fab9343e914ce
Khoi Hoang <hkhoi@outlook.com>
2016-05-23 13:02:32 +0700

    Main windows partially implemented

--- Client/Client/MainWindow.xaml.cs
+++ Client/Client/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -22,7 +23,47 @@ namespace Client
     {
         public MainWindow()
         {
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
             InitializeComponent();
+            tboxQuery.Focus();
+        }
+
+        private String dirPath = "";
+        private void btnBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            /* Select a directory that contains dataset */
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.ShowNewFolderButton = false;
+
+            DialogResult result = dialog.ShowDialog();
+
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                dirPath = dialog.SelectedPath;
+            }
+
+            tboxDirPath.Text = dirPath;
+        }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            /* Extract query string */
+            String queryString = tboxQuery.Text;
+            /* Invalid cases */
+            if (String.IsNullOrWhiteSpace(queryString))
+            {
+                /* Null of empty spaces */
+                System.Windows.MessageBox.Show(this, "Invalid query!", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            else if (String.IsNullOrEmpty(dirPath))
+            {
+                System.Windows.MessageBox.Show(this, "Please select a directory!", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            else
+            {
+                /* Valid cases */
+
+            }
         }
     }
 }
